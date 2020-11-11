0eb670391b5afd9e52825ee9847d6f107b18b8f1
Cake <66772891+cakewhc@users.noreply.github.com>
2020-11-11 16:28:58 +0800

    303 hki hh kc yt

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import routes from 'globals/routes';
 
-import asyncLoadingPage, {asyncLoadingAppUseHooks} from 'components/asyncLoadingComponent';
+import asyncLoadingPage, { asyncLoadingAppUseHooks } from 'components/asyncLoadingComponent';
 // import {library} from '@fortawesome/fontawesome-svg-core';
 import { InfoContextProvider } from 'globals/contexts/infoContext';
 
 import './App.css';
-const App = (props) => {
+const App = props => {
   // constructor(props) {
   //   super(props);
   //   this.getRenderPropForRoute = this.getRenderPropForRoute.bind(this);
   // }
   // componentDidMount() {
   // }
-  const getRenderPropForRoute = (pageName) => {
+  const getRenderPropForRoute = pageName => {
     const AppComponent = asyncLoadingPage(pageName);
     // const AppComponent = asyncLoadingAppUseHooks(pageName);
-    return (match) => { return (<AppComponent match={match} />) };
-  }
+    return match => {
+      return <AppComponent match={match} />;
+    };
+  };
   // render() {
-    return (
-      <div id="App">
-        <InfoContextProvider>
-          <Switch>
-            <Route exact path={routes.exG02A} render={getRenderPropForRoute('exG02A')} />
-            <Route exact path={routes.exG02A2} render={getRenderPropForRoute('exG02A2')} />
-            <Route exact path={routes.exG02B} render={getRenderPropForRoute('exG02B')} />
-            <Route exact path={routes.exG02B2} render={getRenderPropForRoute('exG02B2')} />
-            <Route exact path={routes.ex402} render={getRenderPropForRoute('ex402')} />
-            <Route exact path={routes.ex302} render={getRenderPropForRoute('ex302')} />
-            <Route exact path={routes.ex302A} render={getRenderPropForRoute('ex302A')} />
-            <Route exact path={routes.ex303} render={getRenderPropForRoute('ex303')} />
-            {/* <Route exact path={routes.ex302B} render={getRenderPropForRoute('ex302B')} /> */}
-            <Redirect to={routes.ex303} />
-          </Switch>
-        </InfoContextProvider>
-      </div>
-    );
+  return (
+    <div id='App'>
+      <InfoContextProvider>
+        <Switch>
+          <Route exact path={routes.exG02A} render={getRenderPropForRoute('exG02A')} />
+          <Route exact path={routes.exG02A2} render={getRenderPropForRoute('exG02A2')} />
+          <Route exact path={routes.exG02B} render={getRenderPropForRoute('exG02B')} />
+          <Route exact path={routes.exG02B2} render={getRenderPropForRoute('exG02B2')} />
+          <Route exact path={routes.ex402} render={getRenderPropForRoute('ex402')} />
+          <Route exact path={routes.ex302} render={getRenderPropForRoute('ex302')} />
+          <Route exact path={routes.ex302A} render={getRenderPropForRoute('ex302A')} />
+          <Route exact path={routes.ex303HKI} render={getRenderPropForRoute('ex303HKI')} />
+          <Route exact path={routes.ex303KC} render={getRenderPropForRoute('ex303KC')} />
+          <Route exact path={routes.ex303HH} render={getRenderPropForRoute('ex303HH')} />
+          <Route exact path={routes.ex303YT} render={getRenderPropForRoute('ex303YT')} />
+          {/* <Route exact path={routes.ex302B} render={getRenderPropForRoute('ex302B')} /> */}
+          <Redirect to={routes.ex303HKI} />
+        </Switch>
+      </InfoContextProvider>
+    </div>
+  );
   // }
-}
+};
 
 export default App;
--- src/globals/routes.js
+++ src/globals/routes.js
@@ -1,11 +1,14 @@
 const routes = {
-  'exG02A': '/exG02A',
-  'exG02A2': '/exG02A2',
-  'exG02B': '/exG02B',
-  'exG02B2': '/exG02B2',
-  'ex302': '/ex302',
-  'ex302A': '/ex302A',
-  'ex303': '/ex303',
-  'ex402': '/ex402',
-}
+  exG02A: '/exG02A',
+  exG02A2: '/exG02A2',
+  exG02B: '/exG02B',
+  exG02B2: '/exG02B2',
+  ex302: '/ex302',
+  ex302A: '/ex302A',
+  ex303HKI: '/ex303HKI',
+  ex303KC: '/ex303KC',
+  ex303HH: '/ex303HH',
+  ex303YT: '/ex303YT',
+  ex402: '/ex402'
+};
 export default routes;
